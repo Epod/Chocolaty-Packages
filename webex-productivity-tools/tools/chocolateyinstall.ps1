1c9025357935823e70a2b032f6c67ead65cd29a6
Epod <epod@linux.com>
2022-01-03 17:01:44 -0500

    Initial commit

---
+++ webex-productivity-tools/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  fileType     = 'MSI'
+
+  url          = 'https://akamaicdn.webex.com/client/webexplugin.msi'
+  checksum     = 'F0D8D872DBB5F8CFA9BEBEB673BD28D262D55203F309A8420ADDFA24A66B5D4C'  
+  checksumType = 'sha256'
+
+  silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
+}
+
+Install-ChocolateyPackage @packageArgs
