34c6d1fb56a0ab5b48cc46ecf2deabc8d933d475
Epod <epod@linux.com>
2022-01-05 00:59:56 -0500

    Added FTK Imager 4.5.0

---
+++ manual/ftk-imager/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName  		= $env:ChocolateyPackageName
+  fileType     		= 'MSI'
+
+  url          		= 'https://github.com/TallDwarfs/bins/raw/main/FTK/AccessData_FTK_Imager_(x64).msi'
+  checksum     		= '730248FCF06BE70FEC7A9DF97B1860D8F2FA9855E5995AE0FD13C424FA02BD23'  
+  checksumType 		= 'sha256'
+  
+  silentArgs   		= "/quiet"
+}
+
+Install-ChocolateyPackage @packageArgs
